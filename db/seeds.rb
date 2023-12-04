fc8e15874f1a0280e805a654e336d43bf54acaff
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2023-12-04 16:20:48 +0100

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-def generate_pesel(gender, birth_date)
-  year = birth_date.year % 100
-  month = birth_date.strftime('%m').to_i
-  day = birth_date.strftime('%d').to_i
-
-  gender_digit = gender == 'Male' ? rand(1..9).even? ? 2 : 8 : rand(1..9).even? ? 1 : 7
-
-  random_digits = rand.to_s[2..10]
-
-  "#{year}#{month}#{day}#{random_digits}#{gender_digit}"
-end
-
-50.times do
-  birth_date = Faker::Date.birthday(min_age: 18, max_age: 65)
+5.times do
+  # birth_date = Faker::Date.birthday(min_age: 18, max_age: 65)
   gender = Faker::Gender.binary_type
 
   Patient.create(
@@ -26,7 +14,7 @@ end
     last_name: Faker::Name.last_name,
     gender:gender,
     email: Faker::Internet.email,
-    pesel: generate_pesel(gender, birth_date),
+    pesel: FFaker::IdentificationPL.pesel,
     city: Faker::Address.city
   )
 end
